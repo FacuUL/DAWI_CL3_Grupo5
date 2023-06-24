6f2b2ce1bdef3d7de5e3a63d2e695bc9e1d46a4d
FacuUL <facundodioses05@gmail.com>
2023-06-24 15:50:41 -0500

    CreacionProyecto

---
+++ DAWI_CL3_GRUPO5/src/main/java/pe/edu/cibertec/DAWI_CL3_GRUPO5/DawiCl3Grupo5Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_CL3_GRUPO5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3Grupo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3Grupo5Application.class, args);
+	}
+
+}
